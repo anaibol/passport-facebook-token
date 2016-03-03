f99c90c15acef0a8e7c476990d6f4d92d1bc76bf
anaibol <hey@anib.al>
2016-03-03 10:58:41 +0100

    Added token to user

--- src/index.js
+++ src/index.js
@@ -63,6 +63,8 @@ export default class FacebookTokenStrategy extends OAuth2Strategy {
         if (error) return this.error(error);
         if (!user) return this.fail(info);
 
+        user.accessToken = accessToken
+
         return this.success(user, info);
       };
 
